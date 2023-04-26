1997bd737c3615639205beaf7e00ae5781b4a55f
Arsha Mamoozadeh <38948139+Arsha-M@users.noreply.github.com>
2023-04-26 12:07:32 -0400

    Combine with Rithwick's main.ino

--- pins.h
+++ pins.h
@@ -1,13 +1,25 @@
 #ifndef PINS_H
 #define PINS_H
 
-constexpr int PIN_NEO_PIXEL = 4;   // Arduino pin that connects to NeoPixel
+constexpr int PIN_NEO_PIXEL = 4; // Arduino pin that connects to NeoPixel
 
-constexpr int BLUE_PIN = 9;
-constexpr int YELLOW_PIN = 10;
-constexpr int GREEN_PIN = 11;
-constexpr int RED_PIN = 12;
+constexpr int frontTriggerPin = 6;
+constexpr int frontEchoPin = 7;
 
-constexpr int START_PIN = 13;
+constexpr int in4 = 8;  // L298N motor driver input pin 2 for motor B
+constexpr int in3 = 9;  // L298N motor driver input pin 1 for motor B
+constexpr int in1 = 10; // L298N motor driver input pin 1 for motor A
+constexpr int in2 = 11; // L298N motor driver input pin 2 for motor A
+constexpr int enB = 12; // L298N motor driver enable pin for motor B
+constexpr int enA = 13; // L298N motor driver enable pin for motor A
+
+constexpr int START_PIN = 22;
+
+constexpr int RED_PIN = 23;
+constexpr int GREEN_PIN = 24;
+constexpr int BLUE_PIN = 25;
+constexpr int YELLOW_PIN = 26;
+
+void pinSetup();
 
 #endif // PINS_H
